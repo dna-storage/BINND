501a745bbb9e0893021ba5496dc5554d6c079332
gbrihad <gbrihad@ncsu.edu>
2025-08-05 13:12:58 -0400

    some minor updates to notebooks

--- src/tuning/tuning.py
+++ src/tuning/tuning.py
@@ -8,6 +8,67 @@ from ray.tune.schedulers import ASHAScheduler
 from networks.cnn import GeneralCNNBinaryClassifier
 from dataloader.dataset import HTPDataset
 
+def train_binnd(config, data_directory, max_num_epochs, max_seq_length, device):
+    batch_size = config['batch_size']
+    encode_fn_name = config['encode_fn_name']
+    learning_rate = config['lr']
+
+    train_dataset = HTPDataset(data_path=os.path.join(
+        data_directory, 'train.csv'), max_seq_length=max_seq_length, encode_fn_name=encode_fn_name)
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    val_dataset = HTPDataset(data_path=os.path.join(
+        data_directory, 'val.csv'), max_seq_length=max_seq_length, encode_fn_name=encode_fn_name)
+    val_dataloader = DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+
+
+    model = GeneralCNNBinaryClassifier(
+        input_channels=config['input_channels'],
+        conv2d_layers_config=config['conv2d_config'],
+        conv1d_layers_config=config['conv1d_config'],
+        linear_layers_config=config['linear_config']
+    ).to(device)
+
+    print(model)
+    criterion = nn.BCELoss()  # Binary Cross-Entropy loss
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    
+    for epoch in range(max_num_epochs):
+        
+        model.train()
+        for i_batch, sample_batched in enumerate(train_dataloader):
+            inputs, labels = sample_batched['matrix'].to(device), sample_batched['label'].to(device)
+            
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs.squeeze(), labels)
+            loss.backward()
+            optimizer.step()
+        
+        valid_loss = 0.0
+        correct = 0
+        model.eval()
+        
+        for i_batch, sample_batched in enumerate(val_dataloader):
+            inputs, labels = sample_batched['matrix'].to(device), sample_batched['label'].to(device)
+            
+            outputs = model(inputs)
+            loss = criterion(outputs.squeeze(), labels)
+            valid_loss += loss.item() * inputs.size(0)
+
+            # Compute accuracy
+            preds = (outputs.squeeze() >= 0.5).float()  # Convert to binary predictions
+            correct += (preds == labels).sum().item()
+            
+        
+        metrics = {
+            "loss": valid_loss / len(val_dataloader.dataset),
+            "accuracy": correct /  len(val_dataloader.dataset),
+        }
+        tune.report(metrics)
+    print("Finished Training!")
 
 def main(data_directory, tune_log_dir, max_num_epochs=6, patience=2, reduction_factor=2, num_trials=10):
 
@@ -44,7 +105,7 @@ def main(data_directory, tune_log_dir, max_num_epochs=6, patience=2, reduction_f
         'linear_config': linear_config,
         # 'lr': tune.loguniform(1e-4, 1e-1),
         'lr':tune.choice([0.0004]),
-        'batch_size': tune.choice([128, 256, 512, 1024]),
+        'batch_size': tune.choice([512, 1024]),
         'encode_fn_name': tune.choice(['4xn_v2'])
     }
 
@@ -90,70 +151,7 @@ def main(data_directory, tune_log_dir, max_num_epochs=6, patience=2, reduction_f
     print(f"Best trial config: {best_result.config}")
     print(f"Best trial final validation loss: {best_result.metrics['loss']}")
     print(f"Best trial final validation accuracy: {best_result.metrics['accuracy']}")
-
-def train_binnd(config, data_directory, max_num_epochs, max_seq_length, device):
-    batch_size = config['batch_size']
-    encode_fn_name = config['encode_fn_name']
-    learning_rate = config['lr']
-
-    train_dataset = HTPDataset(data_path=os.path.join(
-        data_directory, 'train.csv'), max_seq_length=max_seq_length, encode_fn_name=encode_fn_name)
-    train_dataloader = DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-
-    val_dataset = HTPDataset(data_path=os.path.join(
-        data_directory, 'val.csv'), max_seq_length=max_seq_length, encode_fn_name=encode_fn_name)
-    val_dataloader = DataLoader(
-        val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
-
-
-    model = GeneralCNNBinaryClassifier(
-        input_channels=config['input_channels'],
-        conv2d_layers_config=config['conv2d_config'],
-        conv1d_layers_config=config['conv1d_config'],
-        linear_layers_config=config['linear_config']
-    ).to(device)
-
-    print(model)
-    criterion = nn.BCELoss()  # Binary Cross-Entropy loss
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     
-    for epoch in range(max_num_epochs):
-        
-        model.train()
-        for i_batch, sample_batched in enumerate(train_dataloader):
-            inputs, labels = sample_batched['matrix'].to(device), sample_batched['label'].to(device)
-            
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs.squeeze(), labels)
-            loss.backward()
-            optimizer.step()
-        
-        valid_loss = 0.0
-        correct = 0
-        total_samples = 0
-        model.eval()
-        
-        for i_batch, sample_batched in enumerate(val_dataloader):
-            inputs, labels = sample_batched['matrix'].to(device), sample_batched['label'].to(device)
-            
-            outputs = model(inputs)
-            loss = criterion(outputs.squeeze(), labels)
-            valid_loss += loss.item() * inputs.size(0)
-
-            # Compute accuracy
-            preds = (outputs.squeeze() >= 0.5).float()  # Convert logits to binary predictions
-            correct += (preds == labels).sum().item()
-            total_samples += inputs.size(0)
-        
-        metrics = {
-            "loss": valid_loss / len(val_dataloader),
-            "accuracy": correct / total_samples,
-        }
-        tune.report(metrics)
-    print("Finished Training!")
-
 if __name__ == '__main__':
     main(
         "/gpfs_backup/tuck_data/gbrihad/DNABindML/data/tuning",
